649e824ab2f5b1b4cd7426913317c29369ec989f
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-12-12 11:27:50 +0100

    initialize project

---
+++ src/main/java/com/example/holidaywishapp/HolidaywishappApplication.java
@@ -0,0 +1,13 @@
+package com.example.holidaywishapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaywishappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidaywishappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/holidaywishapp/HolidaywishappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holidaywishapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaywishappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
